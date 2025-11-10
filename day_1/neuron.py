1bb4b749142b84a76ff2e3f5857485837a295647
349028266 <68523033+349028266@users.noreply.github.com>
2025-11-10 10:45:08 +0000

    cool

--- day_1/neuron.py
+++ day_1/neuron.py
@@ -5,8 +5,9 @@ def sigmoid(x):
   # Our activation function: f(x) = 1 / (1 + e^(-x))
   return 1 / (1 + np.exp(-x))
 
-class Neuron:
-  def __init__(self,weights,bias):
+
+class Neuron: #define a Neuron class
+  def __init__(self,weights,bias): #define
     self.bias = bias
     self.weights = weights
 
